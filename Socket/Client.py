41a456675443827eba69d508264280955e7b7141
root <root@CentOS-Host.localdomain>
2019-12-19 11:18:02 +0800

    [fix] Fix error in socket cconnection

--- Socket/Client.py
+++ Socket/Client.py
@@ -1,29 +1,19 @@
 import socket
 
 class Talker():
-    def __init__(self,port=19991):
-        # initialize
-        # @port: the default port is 19991
-
+    def __init__(self,host,port=19991):
         self.s = socket.socket()
-
-        self.host = socket.gethostname()
-
-        self.s.bind((self.host,port))
-        self.s.listen(5)
+        self.host = host
+        self.port = port
 
     def talk(self,msg):
-        # send a msg to the client throw port 19991
-        # @msg: the msg you want to send
-
-        # c should be the network connection
-        c,addr = self.s.accept()
-
-        print("connect from: ",addr)
-        c.send(msg)
-        c.close()
+        # link establish
+        self.s.connect((self.host, self.port))
+        
+        self.s.send(msg)
+        self.s.close()
 
 if __name__ == "__main__":
     # test case
-    server = Talker()
+    server = Talker("centos-host.local")
     server.talk("Hello world")
