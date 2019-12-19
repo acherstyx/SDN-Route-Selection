c4a583c7b88127acd34f8d3a59437d096068ba24
root <root@CentOS-Host.localdomain>
2019-12-19 10:43:44 +0800

    [fix] Change the name of the file

--- Socket/Client.py
+++ Socket/Client.py
@@ -1,17 +1,29 @@
 import socket
 
-class Listener():
-    def __init__(self,host,port=19991):
+class Talker():
+    def __init__(self,port=19991):
+        # initialize
+        # @port: the default port is 19991
+
         self.s = socket.socket()
-        self.host = host
-        self.port = port
-    def listen(self):
-        self.s.connect((self.host, self.port))
-        msg = self.s.recv(1024)
-        self.s.close()
-        return msg
+
+        self.host = socket.gethostname()
+
+        self.s.bind((self.host,port))
+        self.s.listen(5)
+
+    def talk(self,msg):
+        # send a msg to the client throw port 19991
+        # @msg: the msg you want to send
+
+        # c should be the network connection
+        c,addr = self.s.accept()
+
+        print("connect from: ",addr)
+        c.send(msg)
+        c.close()
 
 if __name__ == "__main__":
     # test case
-    client = Listener("centos-host.local")
-    print(client.listen())
+    server = Talker()
+    server.talk("Hello world")
--- Socket/Server.py
+++ Socket/Server.py
@@ -1,30 +1,17 @@
 import socket
 
-class Talker():
-    def __init__(self,port=19991):
-        # initialize
-        # @port: the default port is 19991
-
+class Listener():
+    def __init__(self,host,port=19991):
         self.s = socket.socket()
-
-        self.host = socket.gethostname()
-
-        self.s.bind((self.host,port))
-        self.s.listen(5)
-
-    def talk(self,msg):
-        # send a msg to the client throw port 19991
-        # @msg: the msg you want to send
-
-        # c should be the network connection
-        c,addr = self.s.accept()
-
-        print("connect from: ",addr)
-        c.send(msg)
-        c.close()
+        self.host = host
+        self.port = port
+    def listen(self):
+        self.s.connect((self.host, self.port))
+        msg = self.s.recv(1024)
+        self.s.close()
+        return msg
 
 if __name__ == "__main__":
     # test case
-    server = Talker()
-    server.talk("Hello world")
-    
+    client = Listener("centos-host.local")
+    print(client.listen())
