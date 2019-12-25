7436e02cb813b44f4c90de741a7eba19f2fe1b0c
ZER0-Nu1L <wcb534813346@126.com>
2019-12-25 09:27:46 +0800

    [feat] valid check

--- interface/Project.py
+++ interface/Project.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from MainWin import Ui_MainWindow
 import numpy as np
+import random
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """
@@ -14,26 +15,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.matplotlibwidget_dynamic.setVisible(True)
         self.matplotlibwidget_dynamic.mpl.update_figure()
-
-    # 信号与槽
+        # 信号与槽
         self.Button_Connect.clicked.connect(self.ConnectTextMsg)
-        self.Button_Connect.clicked.connect(self.ConnectChangeTreeMsg)
+        self.Button_Connect.clicked.connect(self.ConnectTreeMsg)
         self.Button_Break.clicked.connect(self.BreakTextMsg)
-        self.Button_Connect.clicked.connect(self.BreakChangeTreeMsg)
+        self.Button_Connect.clicked.connect(self.BreakTreeMsg)
+    # 简单的输入合法性检查
+    def invalid_check(self):
+        check1 = (self.lineEdit.text()[:9] == 'openflow:') and (self.lineEdit_2.text()[:9] == 'openflow:')
+        check2 = (len(self.lineEdit.text()) == 10) and (len(self.lineEdit_2.text()) == 10)
+        return (check1 and check2)
+
     # TODO:
-    def TextMsg(self):
-        print("up:")
-        print(self.lineEdit.text(), end="")
-        print(self.lineEdit_2.text(), end="")
+    def ConnectTextMsg(self):
+        if self.invalid_check():
+            print("up:")
+            print(self.lineEdit.text(), end="")
+            print(", ")
+            print(self.lineEdit_2.text(), end="")
 
     def BreakTextMsg(self):
-        print("down:")
-        print(self.lineEdit.text(), end="")
-        print(self.lineEdit_2.text(), end="")
+        if self.invalid_check():
+            print("down:")
+            print(self.lineEdit.text(), end="")
+            print(", ")
+            print(self.lineEdit_2.text(), end="")
 
-    def ConnectChangeTreeMsg(self):
+    def ConnectTreeMsg(self):
         pass
-    def BreakChangeTreeMsg(self):
+    def BreakTreeMsg(self):
         pass
 
 if __name__ == "__main__":
